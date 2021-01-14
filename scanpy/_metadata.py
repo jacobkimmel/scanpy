1d871a7a8cfe2e4a4ee3e99fc4a71c0d630ad0fb
Philipp A <flying-sheep@web.de>
2021-01-14 14:45:25 +0100

    Circumvent setuptools bug

--- scanpy/_metadata.py
+++ scanpy/_metadata.py
@@ -3,6 +3,23 @@ from pathlib import Path
 
 here = Path(__file__).parent
 
+
+def refresh_entry_points():
+    """\
+    Under some circumstances, (e.g. when installing a PEP 517 package via pip),
+    pkg_resources.working_set.entries is stale. This tries to fix that.
+    """
+    try:
+        import sys
+        import pkg_resources
+
+        ws: pkg_resources.WorkingSet = pkg_resources.working_set
+        for entry in sys.path:
+            ws.add_entry(entry)
+    except Exception:
+        pass
+
+
 try:
     from setuptools_scm import get_version
     import pytoml
@@ -10,6 +27,7 @@ try:
     proj = pytoml.loads((here.parent / 'pyproject.toml').read_text())
     metadata = proj['tool']['flit']['metadata']
 
+    refresh_entry_points()
     __version__ = get_version(root='..', relative_to=__file__)
     __author__ = metadata['author']
     __email__ = metadata['author-email']
